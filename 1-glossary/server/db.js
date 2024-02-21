b058f5b5e79daec62412dc0946fd12345e767823
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-02-21 10:09:28 -1000

    mongoDB connection established

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/dictionary')
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
+const { Schema } = mongoose
+
+const glossarySchema = new Schema( {
+  word: String,
+  definition: String,
+})
+
+const glossary = mongoose.model(process.env.DB_NAME, glossarySchema)
 // 3. Export the models
+
+module.exports = glossary
 // 4. Import the models into any modules that need them
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const glossary = require('./db.js')
 
 const app = express();
 
 // Serves up all static and generated assets in in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(express.json())
 
 /****
  *
@@ -14,6 +17,20 @@ app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
  *
  *
  */
+app.post('/test', (req, res) => {
+  let entry = new glossary({
+    word: req.body.word,
+    definition: req.body.definition
+  });
+  entry.save()
+  .then(() => {
+    res.sendStatus(200);
+  })
+  .catch((err) => {
+    console.error('Error saving entry ', err);
+    res.sendStatus(500);
+  })
+})
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
