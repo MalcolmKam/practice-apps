5ad9522720b28a936301d2ea912f870b8894c129
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-02-23 20:44:20 -1000

    setting up last page layout

--- 2-checkout/client/src/components/summary.jsx
+++ 2-checkout/client/src/components/summary.jsx
@@ -1,5 +1,11 @@
 import React, {useEffect, useState} from 'react';
 
+let profileData = undefined;
+let shippingData = undefined;
+let billingData = undefined;
+
+//set up axios get requests and set data variables as the response.data
+
 const Summary = () => {
   return (
     <div>
